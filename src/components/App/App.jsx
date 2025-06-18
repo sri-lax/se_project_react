95547ad8029bcfb45770d791c2aaa4d511fc88a2
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-06-18 15:52:59 -0400

    completed part 5

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Main from "../Main/Main";
 import Header from "../Header/Header";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="page">
       <div className="page__content">
         <Header />
+        <Main />
       </div>
     </div>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+      </section>
+    </main>
+  );
+}
+export default Main;
