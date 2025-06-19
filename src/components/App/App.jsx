072ffded31aee6f72ad367c035e1950c30557621
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-06-18 20:14:37 -0400

    completed part 7

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import { useState } from "react";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
+import ItemCard from "../ItemCard/ItemCard";
 import "./Main.css";
+import { defaultClothingItems } from "../../utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
-function Main() {
+function Main({ weatherData }) {
   return (
     <main>
       <WeatherCard />
@@ -8,6 +10,15 @@ function Main() {
         <p className="cards__text">
           Today is 75 &deg; F / You may want to wear:
         </p>
+        <ul className="cards__list">
+          {defaultClothingItems
+            // .filter((item) => {
+            //   return item.weather === weatherData.type;
+            // })
+            .map((item) => {
+              return <ItemCard key={item._id} item={item} />;
+            })}
+        </ul>
       </section>
     </main>
   );
