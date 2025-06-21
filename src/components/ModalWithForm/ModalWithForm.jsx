c13ef6857b240f2b0c6ffaa63d06bc051e85e9c1
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-06-21 00:03:58 -0400

    code review

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -3,10 +3,10 @@ import closeButton from "../../assets/closeButton.svg";
 function ModalWithForm({ children, buttonText, title, activeModal, onclose }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
-      <div className="modal__content">
+      <div className="modal__content modal__content_type_image">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onclose} type="button" className="modal__close">
-          <img src={closeButton} alt="close" />
+          <img src={closeButton} alt="close" className="close__button" />
         </button>
         <form className="modal__form">
           {children}
