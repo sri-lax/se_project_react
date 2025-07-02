fe46d568107a086e04a230aeeb5059cf0a348ede
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-07-02 16:25:28 -0400

    completed part 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,7 @@ import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
 import { getWeather, filterWeatherData } from "../../utils/weatherApi";
+import CurrentTemperatureUnitContext from "../Contexts/CurrentTemperatureUnitContext";
 import Footer from "../Footer/Footer";
 
 function App() {
@@ -16,7 +17,11 @@ function App() {
   });
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
+  const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
 
+  const handleToggleSwitchChange = () => {
+    setCurrentTemperatureUnit(currentTemperatureUnit === "F" ? "C" : "F");
+  };
   const handleCardClick = (card) => {
     setActiveModal("preview");
     setSelectedCard(card);
@@ -56,78 +61,82 @@ function App() {
   }, []);
 
   return (
-    <div className="page">
-      <div className="page__content">
-        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
-        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+    <CurrentTemperatureUnitContext.Provider
+      value={{ currentTemperatureUnit, handleToggleSwitchChange }}
+    >
+      <div className="page">
+        <div className="page__content">
+          <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+          <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        </div>
+        <ModalWithForm
+          isOpen={activeModal === "add-garment"}
+          buttonText="Add garment"
+          title="New garment"
+          activeModal={activeModal}
+          onclose={closeActiveModal}
+        >
+          <label htmlFor="name" className="modal__label">
+            Name{""}
+            <input
+              type="text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
+          </label>
+
+          <label htmlFor="imageUrl" className="modal__label">
+            Image{""}
+            <input
+              type="text"
+              className="modal__input"
+              id="imageUrl"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type</legend>
+
+            <label id="hot" className="modal__label modal__label_type_radio">
+              <input
+                type="radio"
+                className="modal__radio-input"
+                name="weatherType"
+                value="hot"
+              />
+              Hot
+            </label>
+
+            <label id="warm" className="modal__label modal__label_type_radio">
+              <input
+                type="radio"
+                className="modal__radio-input"
+                name="weatherType"
+                value="warm"
+              />
+              Warm
+            </label>
+
+            <label id="cold" className="modal__label modal__label_type_radio">
+              <input
+                type="radio"
+                className="modal__radio-input"
+                name="weatherType"
+                value="cold"
+              />
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
+        <ItemModal
+          activeModal={activeModal}
+          card={selectedCard}
+          onclose={closeActiveModal}
+        />
+        <Footer />
       </div>
-      <ModalWithForm
-        isOpen={activeModal === "add-garment"}
-        buttonText="Add garment"
-        title="New garment"
-        activeModal={activeModal}
-        onclose={closeActiveModal}
-      >
-        <label htmlFor="name" className="modal__label">
-          Name{""}
-          <input
-            type="text"
-            className="modal__input"
-            id="name"
-            placeholder="Name"
-          />
-        </label>
-
-        <label htmlFor="imageUrl" className="modal__label">
-          Image{""}
-          <input
-            type="text"
-            className="modal__input"
-            id="imageUrl"
-            placeholder="Image URL"
-          />
-        </label>
-        <fieldset className="modal__radio-buttons">
-          <legend className="modal__legend">Select the weather type</legend>
-
-          <label id="hot" className="modal__label modal__label_type_radio">
-            <input
-              type="radio"
-              className="modal__radio-input"
-              name="weatherType"
-              value="hot"
-            />
-            Hot
-          </label>
-
-          <label id="warm" className="modal__label modal__label_type_radio">
-            <input
-              type="radio"
-              className="modal__radio-input"
-              name="weatherType"
-              value="warm"
-            />
-            Warm
-          </label>
-
-          <label id="cold" className="modal__label modal__label_type_radio">
-            <input
-              type="radio"
-              className="modal__radio-input"
-              name="weatherType"
-              value="cold"
-            />
-            Cold
-          </label>
-        </fieldset>
-      </ModalWithForm>
-      <ItemModal
-        activeModal={activeModal}
-        card={selectedCard}
-        onclose={closeActiveModal}
-      />
-      <Footer />
-    </div>
+    </CurrentTemperatureUnitContext.Provider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -13,10 +14,11 @@ function Header({ handleAddClick, weatherData }) {
       <p className="header__date-and-location">
         {currentDate},{weatherData.city}
       </p>
-      <button onClick={handleAddClick} className="header__add-clothes-btn">
-        + Add clothes
-      </button>{" "}
       <div type="button" className="header__user-container">
+        <ToggleSwitch />
+        <button onClick={handleAddClick} className="header__add-clothes-btn">
+          + Add clothes
+        </button>{" "}
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
       </div>
